f9f001869cde80671057ffa9d90fc0b2b7b5462d
Dolapo001 <adedolapovictoria927@gmail.com>
2026-06-24 23:39:21 +0100

    feat: configure database settings to support DATABASE_URL environment variable via dj-database-url

--- backend/ruminate/settings.py
+++ backend/ruminate/settings.py
@@ -46,7 +46,11 @@ TEMPLATES = [{
 }]
 WSGI_APPLICATION = "ruminate.wsgi.application"
 
-DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": BASE_DIR / "db.sqlite3"}}
+import dj_database_url
+if "DATABASE_URL" in os.environ:
+    DATABASES = {"default": dj_database_url.config(conn_max_age=600, ssl_require=True)}
+else:
+    DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": BASE_DIR / "db.sqlite3"}}
 
 AUTH_PASSWORD_VALIDATORS = []
 LANGUAGE_CODE = "en-us"
